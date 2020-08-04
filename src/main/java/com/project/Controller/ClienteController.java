4359a384f289aab541157a36abd1a43124999562
Mario23junior <mariojunior3251@gmail.com>
2020-08-04 05:18:11 -0300

    Salvando clientes no banco de dados

---
+++ src/main/java/com/project/Controller/ClienteController.java
@@ -0,0 +1,30 @@
+package com.project.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.Repository.ClienteRepository;
+import com.project.model.entity.Cliente;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+ 
+	private final ClienteRepository repository;
+	
+	@Autowired
+	public ClienteController(ClienteRepository repository) {
+		this.repository = repository;
+	}
+	
+	// salvando no banco de dados
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(Cliente cliente) {
+		return repository.save(cliente);
+	}
+}
