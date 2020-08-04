130eb09f42be80a05c19e6597e785e34120c6fa2
Mario23junior <mariojunior3251@gmail.com>
2020-08-04 01:22:20 -0300

    Iniciando projeto de cadastro de clientes e servicos

---
+++ src/main/java/com/project/ClintesApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClintesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClintesApplication.class, args);
+	}
+
+}
