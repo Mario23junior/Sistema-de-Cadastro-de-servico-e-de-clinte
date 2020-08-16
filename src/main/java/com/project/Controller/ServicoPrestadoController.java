d9c2e57c6aaacfb48702f595179a42aed1f7a011
Mario23junior <mariojunior3251@gmail.com>
2020-08-16 10:03:34 -0300

    Salvando servicos prestados no banco de dados

--- src/main/java/com/project/Controller/ServicoPrestadoController.java
+++ src/main/java/com/project/Controller/ServicoPrestadoController.java
@@ -2,12 +2,12 @@ package com.project.Controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,6 +16,7 @@ import com.project.Repository.ServicoPrestadoRepository;
 import com.project.model.entity.Cliente;
 import com.project.model.entity.ServicoPrestado;
 import com.project.rest.dto.ServicoPrestadoDTO;
+import com.project.util.BigDecimalConverter;
  
 @RestController
 @RequestMapping("/api/servicos-prestados")
@@ -23,13 +24,16 @@ public class ServicoPrestadoController {
 		
 	private final ClienteRepository clienteRepository;
 	private final ServicoPrestadoRepository repository;
+	private final BigDecimalConverter bigDecimalConverter;
 	
-	public ServicoPrestadoController(ClienteRepository clienteRepository,ServicoPrestadoRepository repository) {
-		this.clienteRepository = clienteRepository;
+	public ServicoPrestadoController(ClienteRepository clienteRepository,ServicoPrestadoRepository repository,BigDecimalConverter bigDecimalConverter) {
+		this.clienteRepository = clienteRepository; 
 		this.repository = repository;
+		this.bigDecimalConverter = bigDecimalConverter;
  	}
      
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public  ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto) {
 		  LocalDate dataFormater = LocalDate.parse(dto.getData(),DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		  Integer idCliente = dto.getIdCliente();
@@ -44,8 +48,9 @@ public class ServicoPrestadoController {
 	   servicoPrestado.setDescricao(dto.getDescricao());
 	   servicoPrestado.setData(dataFormater);
 	   servicoPrestado.setCliente(cliente);
-	   //servicoPrestado.setValor();
-	   return null;
+	   servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
+	   
+	   return repository.save(servicoPrestado);
 	}
    	 
 }
